bcb7aac3737de4f2d50d766273f36d875cdee0e3
erdi20 <rayadavid158@gmail.com>
2025-03-23 00:46:20 +0700

    tambah halaman 3 5

--- js/routes.js
+++ js/routes.js
@@ -1,5 +1,6 @@
 var routes = [
-  {path: '/', componentUrl:'pages/first.html'},
+  {path: '/', componentUrl:'pages/five.html'},
   {path: '/second', componentUrl:'pages/second.html'},
+  {path: '/third', componentUrl:'pages/third.html'},
   {path: '(.*)', url:'pages/404.html'}
 ]
