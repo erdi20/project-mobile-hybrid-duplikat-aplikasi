613bdb8133401071a7c3e2384f36b84c2de8fe91
erdi20 <rayadavid158@gmail.com>
2025-04-01 19:06:23 +0700

    fix link

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,8 @@
 var routes = [
-  {path: '/', componentUrl:'pages/third.html'},
+  {path: '/', componentUrl:'pages/home.html'},
   {path: '/second', componentUrl:'pages/second.html'},
   {path: '/third', componentUrl:'pages/third.html'},
+  {path: '/empat', componentUrl:'pages/empat.html'},
+  {path: '/enam', componentUrl:'pages/enam.html'},
   {path: '(.*)', url:'pages/404.html'}
 ]
