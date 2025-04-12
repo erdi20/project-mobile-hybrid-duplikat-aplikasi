6cfe09e878d6604c14b393545e6f76f80111e8ed
erdi20 <rayadavid158@gmail.com>
2025-04-12 22:18:10 +0700

    fix link page five

--- js/routes.js
+++ js/routes.js
@@ -3,6 +3,7 @@ var routes = [
   {path: '/second', componentUrl:'pages/second.html'},
   {path: '/third', componentUrl:'pages/third.html'},
   {path: '/empat', componentUrl:'pages/empat.html'},
+  {path: '/five', componentUrl:'pages/five.html'},
   {path: '/enam', componentUrl:'pages/enam.html'},
   {path: '(.*)', url:'pages/404.html'}
 ]
